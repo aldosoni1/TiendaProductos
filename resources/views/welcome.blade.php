8319c284564220af4018f79b24aee88f3431bed0
JOSSELYN TSASNAI PEREZ LEON <joss.leon@comunidad.unam.mx>
2018-04-23 14:05:10 -0500

    hola

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ugib
                 </div>
 
                 <div class="links">
